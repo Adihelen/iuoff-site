572bddcd6419b228830ce69a0a72dfc67c66a8be
Anderson Lima <andersonlimahw@gmail.com>
2018-05-19 18:39:58 -0300

    adiconado home

--- iuoff-app/src/app/app.module.ts
+++ iuoff-app/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { PackagesComponent } from './components/packages/packages.component';
 import { PartnersComponent } from './components/partners/partners.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},  
+  { path: 'home', component: HomeComponent },
+  { path: 'packages', component: PackagesComponent },
+  { path: 'partners', component: PartnersComponent },
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +27,11 @@ import { PartnersComponent } from './components/partners/partners.component';
     PartnersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
