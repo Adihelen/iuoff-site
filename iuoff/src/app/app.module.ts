ea0223b7508fd7c11f25d1c2cc5d011949a9bba3
Anderson Lima <andersonlimahw@gmail.com>
2018-05-19 12:55:56 -0300

    create components

--- iuoff/src/app/app.module.ts
+++ iuoff/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { PackagesComponent } from './components/packages/packages.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PartnersComponent,
+    PackagesComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
