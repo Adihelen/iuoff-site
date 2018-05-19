2fbc496c9868aaf0a498292d1dd38b012445c1c5
Anderson Lima <andersonlimahw@gmail.com>
2018-05-19 16:19:59 -0300

    IMPORT ASSETS AND CREATE PAGES

--- iuoff-app/src/app/app.module.ts
+++ iuoff-app/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { PackagesComponent } from './components/packages/packages.component';
+import { PartnersComponent } from './components/partners/partners.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    PackagesComponent,
+    PartnersComponent
   ],
   imports: [
     BrowserModule
